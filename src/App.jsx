d3fc1e183c3b198e25614b6da7e577a37ff0526a
Thiago Silva <thiagowfer@gmail.com>
2022-06-08 11:06:09 -0300

    add darkmode with context api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { useContext } from 'react'
+import Toggle from './components/toggle/Toggle'
 import Intro from './components/intro/Intro'
 import About from './components/about/About'
 import ProductList from './components/productList/ProductList'
 import Contact from './components/contact/Contact'
+import { ThemeContext } from './context'
 
 const App = () => {
+  const theme = useContext(ThemeContext)
+  const darkMode = theme.state.darkMode
 
   return (
-    <div>
+    <div style={{ backgroundColor: darkMode ? '#222' : '#fff', color: darkMode && 'white', }}>
+      <Toggle />
       <Intro />
       <About />
       <ProductList />
